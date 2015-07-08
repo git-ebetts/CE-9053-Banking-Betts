489e1a938f6ba35b32db3b205e7d7d7c3200f3cd
git-ebetts <ebettsmail@gmail.com>
2015-07-08 16:56:18 -0400

    first commit

---
+++ app/bank_account.js
@@ -0,0 +1,64 @@
+  // module.exports = BankAccount;
+  
+  function BankAccount(config) {
+  this.accountId=config.accountId;
+  this.balance = config.balance;
+  this.locked = false;
+  this.deposit = config.deposit;
+  this.withdraw = config.withdraw;
+  }
+
+  BankAccount.prototype = {
+
+    accountId: function() {
+      if (_.isString(this.BankAccount.accountId === true))
+      {return this.accountId;}
+      else throw("AccountId must be a string");
+    },
+
+    balance: function() {
+      if(_.isNaN(this.balance)){
+      throw("balance must be a number");
+      } else if (this.balance < 0) {
+      throw("balance must be greater or equal to 0");
+      } else if (this.balance >= 0) {
+      return (this.balance);}
+      else return (this.balance===0);
+    },
+
+    locked : function() {
+      if (_.isBoolean(this.BankAccount.locked === false))
+      throw("Locked must be true or false");
+      if (this.balance < 0) {
+      return this.locked === true;
+      } else {return this.locked === false;}
+    },
+
+    deposit: function(amount) {
+      if(isNaN(this.deposit(amount))){
+      throw("amount must be a number");
+      } else if (this.deposit(amount) <= 0) {
+      throw("deposit must be greater or equal to 0");
+      } else if (this.locked === true) {
+      throw("account is locked");
+      } else {
+      this.balance() === (this.balance() + this.deposit(amount));}
+    },
+
+    withdraw: function(amount) { 
+      if(isNaN(withdraw(amount))){
+      throw("amount must be a number");
+      } else if (this.withdraw(amount) <= 0) {
+      throw("amount must be greater or equal to 0");
+      } else if (this.balance - this.withdraw(amount) <= 1) {
+      throw("insufficient balance");
+      } else if (this.locked === true) {
+      throw("account is locked");
+      } else if (this.balance < 1000) {
+      this.withdraw(amount) === this.withdraw(amount) + 1;
+      } else {
+      this.balance = (this.balance - withdraw(amount));}
+    }
+  };
+
+
---
+++ spec/bank_account_spec.js
@@ -0,0 +1,105 @@
+// var BankAccount = require("../app/bank_account.js");
+
+
+describe("BankAccount", function(){
+    it("exists", function(){
+        expect(BankAccount).toBeDefined();
+    });
+});
+
+describe("BankAccount", function() {
+  var accountId, balance, locked;
+  var deposit, withdraw;
+   });
+
+describe ("accountId()", function() {
+it("should throw an exception if account id is not provided", function () {
+expect(function() {
+	bankAcct1.accountId()}).toThrow();
+});
+});
+
+describe ("balance()", function() {
+
+it("should throw an exception if balance is not a number", function () {
+expect(function(){
+	bankAcct1.balance('bar')}).toThrow();
+});
+
+it("should throw an exception if balance is less than 0", function () {
+	expect(function(){
+	bankAcct1.balance('-1')}).toThrow();
+});
+
+it("should default to 0", function () {
+	expect(function(){
+	bankAcct1.balance()}).toBeDefined(0);
+});
+});
+
+describe ("locked()", function() {
+
+it("should return true or false", function () {
+expect(function(){
+	bankAcct1.balance('foo')}).toThrow;
+});
+
+it("should return true if balance is negative", function () {
+expect(function(){
+	bankAcct1.balance('-1')}).toBeDefined(true);
+});
+
+it("should default to false", function () {
+expect(function(){
+	bankAcct1.locked()}).toBeDefined(false);
+});
+});
+
+describe ("deposit()", function() {
+
+it("should throw an exception if deposit is not a number", function () {
+expect(function(){
+	bankAcct1.deposit('z')}).toThrow();
+});
+
+it("should throw an exception if deposit amount is 0 or less", function () {
+expect(function(){
+	bankAcct1.deposit(0)}).toThrow();
+});
+
+it("should throw an exception if account is locked", function () {
+expect(function(){
+	bankAcct1.locked(true)}).toThrow();
+});
+});
+
+describe ("withdraw()", function() {
+	// var withdrawbalanceBefore
+	// var withdrawbalanceAfter
+
+it("should throw an exception if withdraw amount is not a number", function () {
+expect(function(){
+	bankAcct1.withdraw('z')}).toThrow();
+});
+
+it("should throw an exception if withdraw is 0 or less", function () {
+expect(function(){
+	bankAcct1.withdraw(0)}).toThrow();
+});
+
+it("should throw an exception if withdraw results in negative balance", function () {
+expect(function(){
+	bankAcct1.withdraw(10) = bankAcct1.balance(0)}).toThrow();
+});
+
+it("should throw an exception if account is locked", function () {
+expect(function(){
+	bankAcct1.locked(true)}).toThrow();
+});
+});
+
+
+// it("should deduct 1 if balance is less than 1000", function () {
+// 	expect(function(){
+// 	bankAcct1.withdrawbalanceBefore()-1}).toEqual(bankAcct1.withdrawbalanceAfter();
+// });
